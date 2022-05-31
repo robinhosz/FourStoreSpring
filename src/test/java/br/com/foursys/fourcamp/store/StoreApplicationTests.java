2941ceb4b6fb61bab5a2122b8e0e30465565713f
José Robson Siqueira Júnior <jrobson@server01.foursys.local>
2022-05-31 17:40:19 -0300

    Project Created

---
+++ src/test/java/br/com/foursys/fourcamp/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foursys.fourcamp.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
